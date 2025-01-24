65362255972caae9698f3143c42076f61284d8ee
gargmanya1310 <gargmanya1310@gmail.com>
2025-01-24 14:24:41 +0530

    test insert method

--- src/StringTest.java
+++ src/StringTest.java
@@ -2,8 +2,20 @@ public class StringTest
 {
     public void showName()
     {
-        StringBuilder name = new StringBuilder("John");
-        name.append("Wayne");
+//        StringBuilder name = new StringBuilder("John");
+//        name.append("Wayne");
+//        System.out.println(name);
+//
+//        // chaining with append()
+//        name.append(1).append(true);
+//        System.out.println(name);
+
+        // insert()
+        StringBuilder name = new StringBuilder("John Wayne");
+        name.insert(5, "D. ");
         System.out.println(name);
+
+
+
     }
 }
