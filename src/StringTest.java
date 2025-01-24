783e3624f369adea5542661ae2c6be378c901431
gargmanya1310 <gargmanya1310@gmail.com>
2025-01-24 12:56:13 +0530

    Test String Builder Class 1

---
+++ src/StringTest.java
@@ -0,0 +1,9 @@
+public class StringTest
+{
+    public void showName()
+    {
+        StringBuilder name = new StringBuilder("John");
+        name.append("Wayne");
+        System.out.println(name);
+    }
+}
