bfc14acf0b3335c25900385cc6e1ae7a8709baee
gargmanya1310 <gargmanya1310@gmail.com>
2025-01-27 10:55:08 +0530

    Test array sort

---
+++ src/ArrayOps.java
@@ -0,0 +1,11 @@
+import java.util.Arrays;
+
+public class ArrayOps
+{
+    public void arrays()
+    {
+        int[] nums=new int[] {3,-1,17};
+        Arrays.sort(nums);
+        System.out.println(Arrays.toString(nums));
+    }
+}
