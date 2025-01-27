81a9f16515f3e306da76353bb13fe00960d278ff
gargmanya1310 <gargmanya1310@gmail.com>
2025-01-27 11:42:49 +0530

    Test array compare and mismatch

--- src/ArrayOps.java
+++ src/ArrayOps.java
@@ -4,8 +4,35 @@ public class ArrayOps
 {
     public void arrays()
     {
-        int[] nums=new int[] {3,-1,17};
-        Arrays.sort(nums);
-        System.out.println(Arrays.toString(nums));
+//        int[] nums=new int[] {3,-1,17};
+//        Arrays.sort(nums);
+//        System.out.println(Arrays.toString(nums));
+
+        int result= Arrays.compare(new int[]{3, 7}, new int[]{3});
+        System.out.println(result);
+
+       int a=Arrays.compare(new int[]{3, 7}, new int[]{3, 7});
+       System.out.println(a);
+
+        int b=Arrays.compare(new String[]{"xy", "John Wayne"}, new String[]{"abc", "Hey!"});
+        System.out.println(b);
+
+        int c=Arrays.compare(new String[]{"John", "Wayne"}, new String[]{"john", "Doe"});
+        System.out.println(c);
+
+        int d=Arrays.compare(new String[]{"ab", "John Wayne"}, null);
+        System.out.println(d);
+
+        int e= Arrays.mismatch(new String[]{"John", "Wayne"}, new String[]{"John", "Doe"});
+        System.out.println(e);
+
+        String[] arr1 = new String[]{"John", "Wayne"};
+        String[] arr2 = new String[]{"John", "Wayne", "The Duke"};
+        int f= Arrays.mismatch(arr1, arr2);
+        System.out.println(f);
+
+       int g= Arrays.mismatch(new int[]{3, -2, 7}, new int[]{3, -2, 7});
+       System.out.println(g);
+
     }
 }
