70c46673eae25485c515c0d1bb728f167ef4f508
gargmanya1310 <gargmanya1310@gmail.com>
2025-02-04 12:32:36 +0530

    Test Static variable

---
+++ TestMethods/src/Main.java
@@ -0,0 +1,33 @@
+class Mobile
+{
+    String brand;
+    int price;
+    static String name;
+
+    public void show()
+    {
+        System.out.println("Brand: " + brand + ", Price: " + price + ", Name: " + name);
+    }
+}
+
+
+
+public class Main
+{
+    public static void main(String[] args)
+    {
+        Mobile obj1= new Mobile();
+        obj1.brand="Apple";
+        obj1.price=300;
+        Mobile.name="SmartPhone";
+
+        Mobile obj2= new Mobile();
+        obj2.brand="Samsung";
+        obj2.price=200;
+        Mobile.name="Smartphone";
+
+        obj1.show();
+        obj2.show();
+
+    }
+}
